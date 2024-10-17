d958848ee8d01b916c5978102881127e479f6ce9
r1c4rd0-b43t4 <r.baeta@correo.unimet.edu.ve>
2024-10-16 20:50:15 -0400

    Update main.java

--- Proyecto/src/Proyecto/main.java
+++ Proyecto/src/Proyecto/main.java
@@ -11,11 +11,8 @@ import Reader.Reader;
  *
  * @author rdbae
  */
-public class Main {
+public class Main{
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Reader read = new Reader();
         read.Read();
