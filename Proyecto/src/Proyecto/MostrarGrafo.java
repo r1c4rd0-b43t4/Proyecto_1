3febba167582f91096f5910f00cef065731ae331
r1c4rd0-b43t4 <r.baeta@correo.unimet.edu.ve>
2024-10-23 17:31:32 -0400

    aja

--- Proyecto/src/Proyecto/MostrarGrafo.java
+++ Proyecto/src/Proyecto/MostrarGrafo.java
@@ -4,6 +4,7 @@
  */
 package Proyecto;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.ui.view.Viewer;
 
@@ -12,5 +13,33 @@ import org.graphstream.ui.view.Viewer;
  * @author rdbae
  */
 public class MostrarGrafo {
+//    public void Mostrar(Grafo grafo){
+//        
+//        Graph graphStream = new MultiGraph("MiGrafo");
+//
+//        for (Nodo nodo : grafo.getNodos()) {
+//            graphStream.addNode(nodo.getId());
+//        }
+//
+//        for (Arista arista : grafo.getAristas()) {
+//            graphStream.addEdge(arista.getId(), arista.getOrigen(), arista.getDestino());
+//        }
+//
+//        // Configurar el estilo del grafo (opcional)
+//        graphStream.setAttribute("ui.stylesheet",
+//            "node {fill-color: red; size: 20px; text-size: 20;} edge {fill-color: black;}");
+//        for (Nodo nodo : grafo.getNodos()) {
+//            graphStream.getNode(nodo.getId()).setAttribute("ui.label", "Nodo " + nodo.getId());
+//        }
+//
+//        // Crear un visualizador y mostrar el grafo
+//        Viewer viewer = graphStream.display();
+//        viewer.enableAutoLayout();
+//    }
+//        
+        
+        
+        
+    
     
 }
