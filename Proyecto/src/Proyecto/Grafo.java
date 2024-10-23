b3871f1dd11bdfc0ab3b339d56d444547df7a838
Rene <renebehrens90@gmail.com>
2024-10-23 16:23:50 -0400

    creargrafo
    jiji

--- Proyecto/src/Proyecto/Grafo.java
+++ Proyecto/src/Proyecto/Grafo.java
@@ -185,7 +185,7 @@ class Grafo {
         int indice;
         ListaSimple lad;
         
-        //añadir parametro linea y ponerlo en el constructor
+        //añadir parametro linea 
         public Vertice(String nombre) {
             this.nombre = nombre;
             this.linea="";
