6962a7bfe2bfb485b59fc4be42c2814f9b2ab81a
r1c4rd0-b43t4 <r.baeta@correo.unimet.edu.ve>
2024-10-24 17:20:10 -0400

    arreglos ui y de main

--- Proyecto/src/Proyecto/main.java
+++ Proyecto/src/Proyecto/main.java
@@ -3,6 +3,7 @@ package Proyecto;
 import Reader.Reader;
 import UI.CargaRed_1;
 
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -14,7 +15,7 @@ import UI.CargaRed_1;
  */
 public class Main{
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         
         CargaRed_1 ui  = new CargaRed_1();
         ui.setLocationRelativeTo(null);
