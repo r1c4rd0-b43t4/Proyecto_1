96a6482d31c7372b94bc74d33a9f1e45bfda836a
luisma-2601 <142758332+luisma-2601@users.noreply.github.com>
2024-10-27 20:01:41 -0400

    Cambios

--- Proyecto/src/UI/CoberturaTotal.java
+++ Proyecto/src/UI/CoberturaTotal.java
@@ -43,6 +43,7 @@ public class CoberturaTotal extends javax.swing.JFrame {
         ConsultarBTN = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -64,14 +65,17 @@ public class CoberturaTotal extends javax.swing.JFrame {
                 ConsultarBTNActionPerformed(evt);
             }
         });
-        getContentPane().add(ConsultarBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, -1, -1));
+        getContentPane().add(ConsultarBTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 90, -1, -1));
 
         jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 270, 130));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 90, 270, 130));
+
+        jLabel2.setText("Paradas no cubiertas por alguna sucursal:");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -228,6 +232,7 @@ public class CoberturaTotal extends javax.swing.JFrame {
     private javax.swing.ButtonGroup buttonGroup2;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
